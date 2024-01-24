eb7c3d2732721dc31fb1bd1eb3fb2f5f565622bc
EuronRomanov <juanpablo.gaona@yahoo.com>
2024-01-24 15:18:12 -0500

    caso de prueba es exitoso del subtotal

--- puntoDeVenta/puntoDeVenta.js
+++ puntoDeVenta/puntoDeVenta.js
@@ -32,6 +32,9 @@ calcularValorTotal = function(){
                 Subtotal esperado: 54
             Si el caso de prueba es exitoso, hacer un commit
          */
+
+
+        
     //6. Invocar a calcularDescuento y lo que devuelve guardar en la variable valorDescuento
     //7. Mostrar el resultado en el componente lblDescuento
         /*

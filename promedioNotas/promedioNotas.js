311652220d24043960682026b51f6e7ea2af1f45
EuronRomanov <juanpablo.gaona@yahoo.com>
2024-01-26 23:19:26 -0500

    reto 32 Evidencia de notas v2

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -4,19 +4,29 @@ calcularPromedioNotas=()=>{
     let nota3;
 
     let promedio;
-
+let mensage;
+let imagen;
 
     nota1=recuperarFlotante("txtNota1");
     nota2=recuperarFlotante("txtNota2");
     nota3=recuperarFlotante("txtNota3");
 
     promedio=calcularPromedio(nota1,nota2,nota3);
-
+    
     cambiarTexto("lblResultado", "Su promedio es "+promedio);
-    if (promedio>7) {
-        
-        cambiarImagen("imgResultado","imagenes/exito.gif");
-    } else {
-        cambiarImagen("imgResultado","imagenes/fracaso.gif");
+    if (promedio>0 && promedio<5) {
+         mensage="REPROBADO";
+          imagen="reprobada";
+    } else  if (promedio>=5 && promedio<=8)  {
+        mensage="BUEN TRABAJO";
+        imagen="buenTrabajo";
+    }else  if (promedio>8 && promedio<=10)  {
+        mensage="EXCELENTE";
+        imagen="excelenteTrabajo";
+    }else{
+        mensage="DATOS INCORRECTOS";
+          imagen="incorrecto";
     }
+    cambiarTexto("lblMensaje", mensage);
+    cambiarImagen("imgResultado","imagenes/"+imagen+".gif");
 }

3d7ac2b2587eb5cb87ebf4077ebf89c0e8ca3176
EuronRomanov <juanpablo.gaona@yahoo.com>
2024-01-30 10:45:42 -0500

    reto 37

---
+++ bucles/ejemploFor.js
@@ -0,0 +1,55 @@
+mostarNumeros=()=>{
+    console.log("antes del for");
+    for (let i = 0; i <4; i++) {
+        console.log(i);
+        
+    }
+
+    console.log("despues del for");
+}
+mostarNumeros2=()=>{
+    console.log("antes del for");
+    for (let i = 1; i <=5; i++) {
+        console.log(i);
+        
+    }
+
+    console.log("despues del for");
+}
+mostarPares=()=>{
+    console.log("antes del for");
+    for (let i = 2; i <=10; i+=2) {
+        console.log(i);
+        
+    }
+
+    console.log("despues del for");
+}
+
+mostarInverso=()=>{
+    console.log("antes del for");
+    for (let i = 10; i>0; i--) {
+        console.log(i);
+        
+    }
+
+    console.log("despues del for");
+}
+hackearNasaPelis=()=>{
+    for (let porcentaje = 0; porcentaje <=100; porcentaje+=10) {
+        console.log("hackeando nasa "+porcentaje+"%");
+        
+    }
+    console.log("La nasa ha sido hackeada XD");
+}
+
+
+mostarImpares=()=>{
+    console.log("antes del for");
+    for (let i = 1; i <=21; i+=2) {
+        console.log(i);
+        
+    }
+
+    console.log("despues del for");
+}

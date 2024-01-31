45b23da80817cd2528761133f48c65a5612c2518
EuronRomanov <juanpablo.gaona@yahoo.com>
2024-01-30 22:35:12 -0500

    paso 0 completado

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,1 +1,13 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+
+esMayuscula=(caracter)=>{
+    let codigo;
+    codigo=caracter.charCodeAt(0);
+
+    
+    if((codigo>=65 && codigo<=90) || (codigo === 209) ||  (codigo === 193) || (codigo === 201) || (codigo === 205) ||  (codigo === 211) ||  (codigo === 218)){
+        return true;
+    }else{
+        return false;
+    }
+}

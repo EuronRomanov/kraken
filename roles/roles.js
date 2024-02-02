4971813f52043b8703812e5254ab56ba822625de
EuronRomanov <juanpablo.gaona@yahoo.com>
2024-02-02 12:37:44 -0500

    agregado la tabl de empleados

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,34 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"1114632033",nombre:"Pedro",apellido:"Jaramillo",sueldo:800.0}
 ]
+
+
+
+mostrarEmpleados=()=>{
+    let contenidoTabla="<table>"+
+    "<tr><th>CEDULA</th>"+
+    "<th>NOMBRE</th>"+
+    "<th>APELLIDO</th>"+
+    "<th>SUELDO</th></tr>";
+    let cmpTabla=document.getElementById("tablaEmpleados");
+    empleados.forEach(empleado => {
+        contenidoTabla+="<tr><td>"+
+        empleado.cedula+"</td><td>"+
+        empleado.nombre+"</td><td>"+
+        empleado.apellido+"</td><td>"+
+        empleado.sueldo+"</td></tr>";
+    });
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML= contenidoTabla; 
+}
 
 mostrarOpcionEmpleado=()=>{
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol=()=>{
